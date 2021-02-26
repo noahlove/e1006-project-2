da31d5eb2b3d967f8b775a4416432121f89bf237
Noah Love <noah.love@columbia.edu>
2021-02-26 14:35:45 -0700

    Created outline and added first direction

---
+++ project2.py
@@ -0,0 +1,32 @@
+#imports?
+
+def select_meal():
+    # ask the user repeatedly to add an item until the user types "done"
+    pizza_or_salad = input("Hello, would you like pizza or salad?")
+    if pizza_or_salad == "pizza" or pizza_or_salad == "Pizza":
+        pizza()
+    elif pizza_or_salad == "salad" or pizza_or_salad == "Salad":
+        salad()
+    else:
+        print("You didn't pick one of the two options")
+
+
+def pizza():
+    print("small medium or large?")
+
+
+def toppings():
+    # ask for toppings repeatedly until the user types "done"
+    print("Add a topping...")
+
+
+def salad():
+    print("Would you like a garden salad or greek salad?")
+
+
+def dressing():
+    print("Please choose a dressing...")
+
+
+select_meal()
+
